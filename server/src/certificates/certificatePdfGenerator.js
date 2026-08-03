963f03b0df91fc437746a577c327efd7ea9f535d
AshokAbacco <ashokgowd34@gmail.com>
2026-08-03 17:09:00 +0530

    update

--- server/src/certificates/certificatePdfGenerator.js
+++ server/src/certificates/certificatePdfGenerator.js
@@ -685,7 +685,11 @@ export async function generateCertificatePdf(type, data, imageBuffers = {}) {
 
   const browser = await puppeteer.launch({
     headless: "new",
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+    ],
   });
 
   try {
